3ef75926fc016dbc7ad025f5d466201a9de3c6b0
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2023-05-22 12:22:03 +0300

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -19,26 +19,26 @@ from api.views import *
 from rest_framework import routers
 from django_rest_passwordreset.views import reset_password_confirm, reset_password_request_token
 router = routers.SimpleRouter()
-# router.register(r'user', UserViewSet)
-# router.register(r'product', ProductViewSet)
+router.register(r'user', UserViewSet)
+router.register(r'product', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/register', RegisterUser.as_view()),
     path('api/v1/', include(router.urls)),
     path('user/register/confirm', Сonfirmation.as_view()),
-    # path('user/login', LoginUser.as_view()),
-    # path('user/contact', ContactView.as_view()),
-    # path('user/contacts', ContactAPIList.as_view()),
-    # path('user/details', DetailUser.as_view()),
+    path('user/login', LoginUser.as_view()),
+    path('user/contact', ContactView.as_view()),
+    path('user/contacts', ContactAPIList.as_view()),
+    path('user/details', DetailUser.as_view()),
     path('user/passwordreset', reset_password_request_token),
     path('user/passwordreset/confirm', reset_password_confirm),
-    # path('shops', ShopView.as_view()),
-    # path('categories', CategoryView.as_view()),
-    # path('products', ProductView.as_view()),
-    # path('partner/update', PartnerUpdate.as_view()),
-    # path('partner/state', PartnerState.as_view()),
-    # path('partner/orders', PartnerOrders.as_view()),
-    # path('cart', CartView.as_view()),
-    # path('order', OrderView.as_view()),
+    path('shops', ShopView.as_view()),
+    path('categories', CategoryView.as_view()),
+    path('products', ProductView.as_view()),
+    path('partner/update', PartnerUpdate.as_view()),
+    path('partner/state', PartnerState.as_view()),
+    path('partner/orders', PartnerOrders.as_view()),
+    path('cart', CartView.as_view()),
+    path('order', OrderView.as_view()),
 ]
